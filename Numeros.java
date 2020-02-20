cf1dcb579ee3f4599b207a865a016ff9d806e6b3
Eduarda-Santos <Eduarda Santos de Andrade>
2020-02-20 08:53:40 -0300

    Numeros pares

--- Numeros.java
+++ Numeros.java
@@ -1,11 +1,18 @@
 public class Numeros{
 
-    public static void main(Int[] args){
+    public static void main(String[] args){
         int num;
 
+        System.out.println("Números divididos por 11 com resto 5:");
         for(num = 1; num < 2500; num++){
-            if(num%11 == 5){
-                    System.out.println("%d", num);
+           if(num%11 == 5){
+                System.out.println(" "+num);
+            }
+        }
+        System.out.println("Números pares:");
+        for(num = 1; num < 2500; num++){
+            if(num%2 == 0){
+                System.out.println(" "+num);
             }
         }
     }
