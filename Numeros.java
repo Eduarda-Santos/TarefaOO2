67b777e903ad7c09f1c1a7e2b3b4c76ee49003b3
Eduarda-Santos <Eduarda Santos de Andrade>
2020-02-20 08:26:48 -0300

    Numero 11 com resto 5

---
+++ Numeros.java
@@ -0,0 +1,12 @@
+public class Numeros{
+
+    public static void main(Int[] args){
+        int num;
+
+        for(num = 1; num < 2500; num++){
+            if(num%11 == 5){
+                    System.out.println("%d", num);
+            }
+        }
+    }
+}
