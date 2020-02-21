b1956ffd0950b6fa922f75269bc6fa1bfb4d5f22
Eduarda-Santos <iameduardas@gmail.com>
2020-02-21 11:02:10 -0300

    Numeros2102.java

--- NPositivos.java
+++ NPositivos.java
@@ -1,17 +1,40 @@
+import java.util.Scanner;
+
 public class NPositivos{
 
     public static void main(String[] args){
-        int n = 0, aux, i;
-        Scanner scan = new Scanner(System.in);
+        int n = 0, aux = 0, i, maior = 0, menor = 0, soma = 0, sub = 0, acres = 0;
+        Scanner scan;
+
+        scan = new Scanner(System.in);
 
         while(n != -1){
-            Sytem.out.println("Digite um num:");
+            System.out.println("Digite um num:");
             n = scan.nextInt();
-            
-            if(n > aux){
-                System.out.println(" "+n);
-            }
-            
+                
+            if(n < 100){
+                if(n > aux){
+                    maior = n;
+                }
+                if(aux > n){
+                    menor = n;
+                }
+                soma = soma + n;
+                sub = maior - n;
+                acres = menor + n;
+                if(n != -1){
+                    aux = n;
+                }
+            }  
+            else{
+            System.out.println("Tente novamente");
+            System.exit(0);
+            }  
         }
+    System.out.println("Maior número digitado: "+maior);
+    System.out.println("Menor número digitado: "+menor);
+    System.out.println("Soma de todos os números digitados: "+soma);
+    System.out.println("Cada número digitado subtraído pelo maior: "+sub);
+    System.out.println("Cada número digitado acrescido pelo menor: "+acres);
     }
 }
--- Numeros.java
+++ Numeros.java
@@ -1,7 +1,7 @@
 public class Numeros{
 
     public static void main(String[] args){
-        int num;
+        int num, cont = 0;
 
         System.out.println("Números divididos por 11 com resto 5:");
         for(num = 1; num < 2500; num++){
@@ -18,6 +18,30 @@ public class Numeros{
         System.out.println("Números ímpares:");
         for(num = 1; num < 2500; num++){
             if(num%2 == 1){
+                System.out.println(" "+num);
+            }
+        }
+        System.out.println("Números primos:");
+        for(num = 1; num < 2500; num++){
+            if(num%2 == 0){
+                cont++;
+            }
+            else if (num%3 == 0){
+                cont++;
+            }
+            else if(num%5 == 0){
+                cont++;
+            }
+            else if(num%7 == 0){
+                cont++;
+            }
+            else if(num%11 == 0){
+                cont++;
+            }
+            else if(num%13 == 0){
+                cont++;
+            }
+            if(cont == 1){
                 System.out.println(" "+num);
             }
         }
